df64773c927a3a3475098ed89220d219cea2966a
JuanEstebanGarcia <juan103steban@gmail.com>
2022-04-21 22:03:16 -0500

    Instale las dependencias correspontientes para correr el programa, agregue un comemtario de auror osea mi nombre y ya el resto de informacion la ingrese en el archivo readme.

---
+++ sistema/src/sistema/Sistema.java
@@ -0,0 +1,37 @@
+
+
+package sistema;
+
+import sistema.empleadosGUI.frmEmpleados;
+
+public class Sistema {
+     public static void main(String args[]) {
+        
+        /* ACTIVIDAD - JUAN ESTEBAN GARCIA */
+      
+    
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmEmpleados.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new frmEmpleados().setVisible(true);
+        });
+    }
+    
+}
